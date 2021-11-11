de94f4291fbc362060f418e76ce3308d704b1caf
Asif-1994 <94127297+Asif-1994@users.noreply.github.com>
2021-11-11 22:51:24 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+
+<html>
+
+
+<head>
+	<title>My Page</title>
+</head>
+
+<body>
+  <!--Image of Udemy-->
+	<h1 id="top">My Name Is Asif Ansari</h1>
+	<!-- 'id' is used for internal links -->
+	<p>My Name is Asif Ansari And Currently Learning HTML and CSS</p>
+	<img src="img/download.jpeg" width="250" alt="Udemy">
+	<h3>Logo of youtube</h3>
+	<a href="https://www.udemy.com/" target="_blank">Visit Udemy for more info.</a>
+				<!-- target _blank will open the link in new window-->
+	<br>
+	<a href="index.html" target="_blank">About</a>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<a href="#top">Back to top</a>
+</body>
+
+
+
+
+
+
+</html>
+
+
+
